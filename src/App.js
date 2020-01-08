2ae3f0caff3d1e8f739250837e500187024d3e4e
George Jone <g.george.jone@gmail.com>
2020-01-07 18:18:09 -0800

    dark theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 import Dashboard from'./dashboard/Dashboard';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 function App() {
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+  
   return (
-    <Dashboard />
+    <ThemeProvider theme={darkTheme}>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
 
