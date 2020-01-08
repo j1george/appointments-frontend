0458332547551d484c103e8783895ffc86fa1cf2
George Jone <g.george.jone@gmail.com>
2020-01-07 20:26:56 -0800

    implement time picker. reformat date picker

--- src/dashboard/AppointmentList.js
+++ src/dashboard/AppointmentList.js
@@ -84,8 +84,6 @@ export default function AppointmentList({doctor, date}) {
 
   const addAppointment = () => {
     (async () => {
-      console.log(patient, appointmentTime, appointmentType);
-
       let result = await axios.get(`http://138.68.16.40:3000/v1/patients?first_name=${patient.first_name}&last_name=${patient.last_name}`);
 
       const patientExists = result.data.length > 0;
@@ -140,7 +138,12 @@ export default function AppointmentList({doctor, date}) {
           <TableRow key={appointments.length}>
             <TableCell>{appointments.length+1}</TableCell>
             <TableCell><TextField label='First Name' onChange={setPatientFirstName} /><TextField label='Last Name' onChange={setPatientLastName} /></TableCell>
-            <TableCell><TextField label='e.g. 9:00AM' onChange={onSetAppointmentTime} /></TableCell>
+            <TableCell>
+              <TextField
+                type='time'
+                onChange={onSetAppointmentTime} 
+              />
+            </TableCell>
             <TableCell>
               <Select
                 value={appointmentType}
